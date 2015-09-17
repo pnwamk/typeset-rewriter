513f0fae313a6cd7fbcebfa8a90612b619b27407
Andrew Kent <andmkent@indiana.edu>
2015-09-17 14:19:03 -0400

    different names
    names that are less likely to conflict w/ redex functions which may
    exist at some point in the future, and use `rw-lambda` instead of `rw`
    since it is so similar to Racket’s `match-lambda`

--- main.rkt
+++ main.rkt
@@ -5,14 +5,14 @@
          (for-syntax racket/base syntax/parse syntax/stx racket/format)
          (for-template racket/base))
 
-(provide with-atomic-rewriters
-         with-rewriters
+(provide with-atomic-rws
+         with-rws
          define-rw-context
-         rw)
+         rw-lambda)
 
 ;; like 'with-compound-rewriters' but
 ;; for atomic rewriters
-(define-syntax (with-atomic-rewriters stx)
+(define-syntax (with-atomic-rws stx)
   (syntax-parse stx
     [(_ () body) #'body]
     [(_ ([x rw] rst ...) body)
@@ -21,7 +21,7 @@
            body))]))
 
 ;; allows for atomic + compound rewriters
-(define-syntax (with-rewriters stx)
+(define-syntax (with-rws stx)
   (define-splicing-syntax-class atomic-rws
     (pattern (~seq #:atomic rws:expr))
     (pattern (~seq) #:with rws #'()))
@@ -47,9 +47,9 @@
   (syntax-parse stx
     [(_ name:id as:atomic-rws cs:compound-rws)
      #'(define-syntax-rule (name body-expr)
-         (with-rewriters #:atomic as.rws
-                         #:compound cs.rws
-                         body-expr))]))
+         (with-rws #:atomic as.rws
+                   #:compound cs.rws
+                   body-expr))]))
 
 ;; matches a redex lw struct
 ;; with the specified symbol
@@ -79,35 +79,20 @@
                        (lw "}" _ _ _ _ #f #f)))
              _ _ _ _ _ _)])))
 
-;; parse the inside of a rw pattern, conerting
-;; each piece into the appropriate match forms
-(define-for-syntax (parse-rw-internal stx)
-  (syntax-parse stx
-    [(~datum ...) stx]
-    [((~literal unquote) match-expr) #'match-expr]
-    [sym:id
-     #'(-literal sym)]
-    [(e ...)
-     #`(-sexp #,@(stx-map parse-rw-internal #'(e ...)))]
-    [_ (raise-syntax-error
-        'rw
-        (~a "unrecognized rw pattern: " (syntax->datum stx))
-        stx)]))
-
 ;; rw
 ;; rewriter builder
 ;; (rw name [`(name pats ...) => output] ...)
 ;; ' is used to declare literal matches
 ;; anything else is matched like a match pattern variable
 ;; output is any expression
-(define-syntax (rw stx)
+(define-syntax (rw-lambda stx)
   (syntax-parse stx
     [(_ [((~literal quasiquote) (name:id . args)) (~datum =>) output]
         cases ...)
      #`(match-lambda
-         #,(rw-case #'name #'[(quasiquote (name . args)) => output])
-         #,@(stx-map (λ (c) (rw-case #'name c)) #'(cases ...))
-         [else (error 'rw "no ~a case for ~a" 'name else)])]
+         #,(parse-rw-lambda-case #'name #'[(quasiquote (name . args)) => output])
+         #,@(stx-map (λ (c) (parse-rw-lambda-case #'name c)) #'(cases ...))
+         [else (error 'rw-lambda "no ~a case for ~a" 'name else)])]
     [_ (raise-syntax-error
         'rw
         (~a "expected: (rw [`(name pats ...) => output] ...)\n got: "
@@ -115,7 +100,12 @@
             "\n")
         stx)]))
 
-(define-for-syntax (rw-case name case)
+;; parses an rw-lambda case
+;; name: identifier? 
+;; case: syntax?
+;;       should be of form [`(rw-name pats ...) => output-exp]
+;; output: syntax?
+(define-for-syntax (parse-rw-lambda-case name case)
   (syntax-parse case
     [[((~literal quasiquote) (cname:id . args)) (~datum =>) output]
      (unless (free-identifier=? name #'cname)
@@ -128,17 +118,17 @@
         case))
      #`[(or (list
              (lw "(" _ _ _ _ #f #f)
-             (-literal cname) #,@(stx-map parse-rw-internal #'args)
+             (-literal cname) #,@(stx-map parse-rw-case-internal #'args)
              (lw ")" _ _ _ _ #f #f))
             
             (list
              (lw "[" _ _ _ _ #f #f)
-             (-literal cname) #,@(stx-map parse-rw-internal #'args)
+             (-literal cname) #,@(stx-map parse-rw-case-internal #'args)
              (lw "]" _ _ _ _ #f #f))
             
             (list
              (lw "{" _ _ _ _ #f #f)
-             (-literal cname) #,@(stx-map parse-rw-internal #'args)
+             (-literal cname) #,@(stx-map parse-rw-case-internal #'args)
              (lw "}" _ _ _ _ #f #f)))
         output]]
     [_ (raise-syntax-error
@@ -147,3 +137,19 @@
             (syntax->datum case)
             "\n")
         case)]))
+
+
+;; parse the inside of a rw-lambda case pattern, conerting
+;; each piece into the appropriate match forms
+(define-for-syntax (parse-rw-case-internal stx)
+  (syntax-parse stx
+    [(~datum ...) stx]
+    [((~literal unquote) match-expr) #'match-expr]
+    [sym:id
+     #'(-literal sym)]
+    [(e ...)
+     #`(-sexp #,@(stx-map parse-rw-case-internal #'(e ...)))]
+    [_ (raise-syntax-error
+        'rw
+        (~a "unrecognized rw pattern: " (syntax->datum stx))
+        stx)]))
